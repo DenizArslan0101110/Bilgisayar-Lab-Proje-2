02692e503db9b74c94126d8993b5d53c9b160ee1
Deniz Arslan <Deniz.Arslan.0101110@gmail.com>
2024-10-31 11:24:19 +0300

    Update Main.java
    pencere açtım lesgo

--- Proje-2-Kart-Oyunu/src/Main.java
+++ Proje-2-Kart-Oyunu/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.Font;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome Yeah!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ImageIcon fumo = new ImageIcon("Visual/djsjdjebfowodbwocbwoc.jpg"); // load image for label
+
+        Border border = BorderFactory.createLineBorder(Color.blue, 3);  // create border
+
+        JLabel label = new JLabel();                                // create label
+        label.setText("Sanırsam bu DrawText fonksiyonu, galiba");   // add text to label
+        label.setIcon(fumo);                                        // add image to label
+        label.setHorizontalTextPosition(JLabel.CENTER);             // center the text to label
+        label.setVerticalTextPosition(JLabel.TOP);                  // make text above label
+        label.setForeground(new Color(223, 250, 20));               // makes label text colored
+        label.setFont(new Font("MV Boli", Font.PLAIN, 20));         // sets font
+        label.setIconTextGap(100);                                  // makes text much above
+        label.setBackground(Color.BLACK);                           // makes label bg colored
+        label.setOpaque(true);                                      // shows label bg
+        label.setBorder(border);                                    // draws border around label
+        label.setVerticalAlignment(JLabel.CENTER);                  // center label in x inside window
+        label.setHorizontalAlignment(JLabel.CENTER);                // center label in y inside window
+
+        JFrame window = new JFrame();                               // create frame
+        window.setTitle("Kart Oyunu: Savaş Araçları");              // set title
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      // x button purpose
+        window.setResizable(false);                                 // disable resize
+        window.setSize(1366, 876);                                  // set size
+        window.setVisible(true);                                    // make window visible
+        window.add(label);                                          // draws our label
+
+        ImageIcon logo = new ImageIcon("Visual/logo.png");           // create image for icon
+        window.setIconImage(logo.getImage());                        // change icon
+        window.getContentPane().setBackground(new Color(255, 0, 0)); // set bg color
+
     }
 }
