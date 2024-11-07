2a0cc076b61ed002c936990a98311943147ca652
Kağan Burhan <kaganburhan@gmail.com>
2024-11-07 16:30:16 +0300

    selam

--- Proje-2-Kart-Oyunu/src/Oyun.java
+++ Proje-2-Kart-Oyunu/src/Oyun.java
@@ -2,6 +2,12 @@ public class Oyun
 {
     public static void main(String[] args)
     {
+        Oyuncu bilgisayar = new Oyuncu();
+        Oyuncu Player = new Oyuncu();
+
+
+
+
     GraphicalUserInterface window = new GraphicalUserInterface();
     window.MainGraphics();
     }
--- Proje-2-Kart-Oyunu/src/Oyuncu.java
+++ Proje-2-Kart-Oyunu/src/Oyuncu.java
@@ -3,4 +3,7 @@ public class Oyuncu
     boolean OyuncuID; // 1 insan, 0 makine
     String OyuncuAdi;
     short skor;
+
+
+
 }
